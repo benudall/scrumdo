251047e07cb547873cb076e862e24e565e292b04
verefor <verefor@googlemail.com>
2016-11-08 17:35:18 +0000

    removed unused modules
    now outputs a formatted response instead of rebuilding into a result
    style changes

--- scrumdo.js
+++ scrumdo.js
@@ -1,12 +1,14 @@
 var https = require('https');
-var request = require('sync-request')
-var fs = require('fs');
 var Handlebars = require('handlebars');
+var fs = require('fs');
 
+
+var before = "2016-11-06";
+var after = "2016-10-31";
 
 var options = {
 	host: 'app.scrumdo.com',
-	path: '/api/v3/organizations/aqueduct1/projects/szki-car-production-j5555/search?q=before:%202016-11-06,%20after:%202016-10-31,%20cell:%20Done',
+	path: '/api/v3/organizations/aqueduct1/projects/szki-car-production-j5555/search?q=before:'+before+',after:'+after+',cell:Done',
 	headers: {
 		'Authorization': 'Basic YmVudTpUb3dlcjEyOQ=='
 	}
@@ -20,11 +22,19 @@ https.get(options, (res) => {
 	res.on('end', () => {
 		response=JSON.parse(rawresponse);
 		console.log("Found "+response.items.length+" tickets");
-		for(x=0;x<response.items.length;x++){
-			console.log(response.items[x].number);
-			console.log(response.items[x].summary);
-			
-		}
+		
+		
+		var template = fs.readFileSync('report.hbs').toString();
+		var compiled = Handlebars.compile(template);
+		//define extra vars for template here
+		response.date = new Date().toJSON().replace(/T/g," ").replace(/:/g,"-").replace(/\.\d{3}Z/g,"");
+		response.start=after;
+		response.end=before;
+		response.items=response.items.sort(function(a,b){return a.number-b.number});
+		
+		fs.writeFileSync("Scrumdo Report "+response.date+".html",compiled(response));
+		console.log("Scrumdo Report "+response.date+".html created");
+		console.log("END");
 	})
 }).on('error', (e) => {
 	console.error(e);
